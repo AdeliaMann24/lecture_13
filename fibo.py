fca8a2fe004621320a60224f1c1a720cdd61b71c
Adelia Mannapova <xmanna00@vutbr.cz>
2021-05-13 00:21:49 +0300

    Add recursive fibo

--- fibo.py
+++ fibo.py
@@ -1,6 +1,20 @@
+def recursive_nth_fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(n - 1) + recursive_nth_fibo(n - 2)
 
 def main():
-    pass
+    n = int(input("Zadejte pocet prvku Fib.posl.: "))
+
+    fib_seq = []
+    for num in range(n + 1):
+        fib_seq.append(recursive_nth_fibo(num))
+
+    fib_seq = [recursive_nth_fibo(num) for num in range(n + 1)]
+    print(fib_seq)
 
 
 if __name__ == '__main__':
